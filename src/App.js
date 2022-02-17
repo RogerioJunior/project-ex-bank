5b0659034bd19cf0ffc2ee9713a7bbf60ac53894
RogerioJunor <rogeriopcjr@gmail.com>
2022-02-17 02:16:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Main from './components/Main';
+
+function App() {
+
+  const [sidebarOpen, setsidebarOpen] = useState(false);
+  const openSidebar = () => {
+    setsidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setsidebarOpen(false);
+  };
+  return (
+    <div className="container">
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+      <Main />
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+    </div>
+  );
+};
+
+export default App;
