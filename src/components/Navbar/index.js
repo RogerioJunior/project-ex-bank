87723cc0f70dd298b76e5a4dc904dc0b1fc00668
RogerioJunor <rogeriopcjr@gmail.com>
2022-02-17 04:24:11 -0300

    navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,4 @@
 import './styles.css';
-import avatar from '../../assets/img/Bank_72.png'
 
 function Navbar({ sidebarOpen, openSidebar }) {
 
@@ -8,26 +7,12 @@ function Navbar({ sidebarOpen, openSidebar }) {
             <div className="nav_icon" onClick={() => openSidebar()}>
                 <i className="fa fa-bars" aria-hidden="true"></i>
             </div>
-            <div className="navbar__left">
-                <a href="#">Subscribers</a>
-                <a href="#">Video Management</a>
-                <a className="active_link" href="#">
-                    Admin
-                </a>
-            </div>
             <div className="navbar__right">
-                <a href="#">
-                    <i className="fa fa-search" aria-hidden="true"></i>
-                </a>
-                <a href="#">
-                    <i className="fa fa-clock-o" aria-hidden="true"></i>
-                </a>
-                <a href="#!">
-                    <img width="30" src={avatar} alt="avatar" />
-                </a>
+            <button>Connect</button>
             </div>
         </nav>
     );
 };
 
 export default Navbar;
+
